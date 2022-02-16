3c17fb2edd487e42c72aac0ccb8f04fc54f8ec21
Md Shahriar Islam <mdshahriarislam23@gmail.com>
2022-02-17 03:15:56 +0600

    expensenses and saving calculation added

--- calculation.js
+++ calculation.js
@@ -1,19 +1,35 @@
 document.getElementById('btn1').addEventListener('click', function () {
     const expenses = document.getElementById('totalExpenses');
     const remaingBalance = document.getElementById('remain');
-    const myIncome = myTotalIncome();
+    const balanceRemain = myRemaingBalance();
     const mySpend = mySpendingAmount();
-    let raminingAmount = 0;
-    if (mySpend > myIncome) {
-        alert('this is not possible..get a value nice')
-    }
-    else {
-        remainingAmount = myIncome - mySpend;
-    }
     expenses.innerText = mySpend;
-    remaingBalance.innerText = remainingAmount;
+    remaingBalance.innerText = balanceRemain;
 
 });
+document.getElementById('btn2').addEventListener('click', function () {
+    let savedAmount = document.getElementById('saved');
+    let remaining = document.getElementById('leftAmount');
+    let totalExpenses = document.getElementById('totalExpenses');
+    let savedMoney = mySavings();
+    let moneyLeft = parseFloat(document.getElementById('remain').innerText);
+    let moneyNow = 0
+    if (parseFloat(totalExpenses.innerText) == 0) {
+        moneyNow = myTotalIncome() - savedMoney;
+    }
+    else if (savedMoney > moneyLeft) {
+        alert('you can not save money if you do nont have enough money');
+
+    }
+    else {
+        moneyNow = moneyLeft - savedMoney;
+
+    }
+    savedAmount.innerText = savedMoney;
+    remaining.innerText = moneyNow;
+});
+
+// my total income
 function myTotalIncome() {
     const incomeMoney = document.getElementById('income');
     let incomeAmount = 0;
@@ -56,3 +72,36 @@ function mySpendingAmount() {
     totalSpendAmount = foodAmount + clothAmount + rentAmount;
     return totalSpendAmount;
 };
+// remaining balance after spending money
+function myRemaingBalance() {
+    const myIncome = myTotalIncome();
+    const mySpend = mySpendingAmount();
+    let raminingAmount = 0;
+    if (mySpend > myIncome) {
+        alert('this is not possible..get a value nice')
+    }
+    else {
+        remainingAmount = myIncome - mySpend;
+    }
+    return remainingAmount;
+
+}
+// saved money
+function mySavings() {
+    let savingPercentage = document.getElementById('percentage');
+    let savingPercentageMoney = 1;
+    let TotalIncome = 0;
+    let totalSavedMoney = 1;
+    if (isNaN(parseFloat(savingPercentage.value)) || parseFloat(savingPercentage.value) < 0) {
+        alert('Not a correct value');
+    }
+    else {
+        savingPercentageMoney = parseFloat(savingPercentage.value) / 100;
+
+    }
+    TotalIncome = myTotalIncome();
+    totalSavedMoney = TotalIncome * savingPercentageMoney;
+    return totalSavedMoney;
+
+
+}
